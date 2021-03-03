34e786df22b29c92d0a44f79405e689f66c7d244
RangeHighGuestTeacher <79975450+RangeHighGuestTeacher@users.noreply.github.com>
2021-03-03 11:08:32 +0000

    Instructions and example file added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Hello World Template
  A template for the Hello World assignment
+
+ In Python you can print messages using..
+
+  print("Hello World")
+
+  Run the example file example.py to see this in action.
---
+++ example.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+########## Edit below this line 
+# print your name to the console
+
+
+
+
+
+
+########## Edit above this line
+# Once you've finished save and commit your changes to GitHub
